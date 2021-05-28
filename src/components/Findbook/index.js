63632044357f12ef61275dc662600c7a0726cd03
João Cláudio <jca.olivera91@gmail.com>
2021-05-28 05:19:39 -0300

    preparing components to connect with api

---
+++ src/components/Findbook/index.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import { Container } from './styles'
+
+import axios from 'axios'
+
+
+export function FindBook () {
+    const [book, setBook] = useState('')
+    const [ result, setResult] = useState([])
+
+    function handleChange(event){
+        const book = event.target.value
+
+        setBook(book)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAus8q7Wr92etcjgDB_MvOzblDcRvxnO4M')
+        .then( data => {
+            console.log(data)
+        })
+    }
+
+    return (
+        <Container>
+          <header>
+            <h2>Busque seu Livro <span>Favorito</span></h2>
+
+            <div>
+              <input 
+                type="text" 
+                placeholder="Qual livro ler hoje?"
+                onChange={handleChange}
+              />
+              <button type="submit" onClick={handleSubmit}>
+                <FiSearch size={16} color="#fff"/>
+              </button>
+            </div>
+
+          </header>
+
+          
+    
+        </Container>
+    )    
+}
+
---
+++ src/components/Header/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+
+    background: #718096;
+    padding-top: 2rem;
+  
+    h1 {
+        max-width: 1120px;
+        margin: 0 auto;
+
+        padding: 2rem 1rem 12rem;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        color: #f7a203;
+
+        span { 
+            font-weight: normal;
+            color: var(--shapes); 
+
+        }
+    }
+`
